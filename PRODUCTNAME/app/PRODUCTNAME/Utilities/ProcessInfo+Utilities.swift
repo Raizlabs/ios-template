1d06093f6e778475811e8992297a61ad84a5929c
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2018-04-03 17:59:28 -0400

    Add automatic screenshots via fastlane snapshot.

--- PRODUCTNAME/app/PRODUCTNAME/Utilities/ProcessInfo+Utilities.swift
+++ PRODUCTNAME/app/PRODUCTNAME/Utilities/ProcessInfo+Utilities.swift
@@ -6,14 +6,18 @@
 //  Copyright © 2018 ORGANIZATION. All rights reserved.
 //
 
+import Foundation
+
 extension ProcessInfo {
+
+    static let uiTestsKey = "com.raizlabs.uiTests"
 
     static var isRunningUITests: Bool {
         // If you want to run the app in Debug mode, but have it pretend that it
         // is running UI tests, for purposes of testing with fake data or the like,
         // return true here.
         #if DEBUG
-            return ProcessInfo.processInfo.environment["uiTests"] == "true"
+            return ProcessInfo.processInfo.environment[ProcessInfo.uiTestsKey] == "true"
         #else
             return false
         #endif
---
+++ {{ cookiecutter.project_name | replace(' ', '') }}/app/{{ cookiecutter.project_name | replace(' ', '') }}/Application/AppLifecycle/StatusBarConfiguration.swift
@@ -0,0 +1,26 @@
+//
+//  StatusBarConfiguration.swift
+//  {{ cookiecutter.project_name | replace(' ', '') }}
+//
+//  Created by Zev Eisenberg on 4/3/18.
+//  Copyright © 2018 {{ cookiecutter.company_name }}. All rights reserved.
+//
+
+#if DEBUG
+    import SimulatorStatusMagic
+#endif
+
+struct StatusBarConfiguration: AppLifecycle {
+
+    var isEnabled: Bool {
+        return ProcessInfo.isRunningUITests
+    }
+
+    func onDidLaunch(application: UIApplication, launchOptions: [UIApplicationLaunchOptionsKey: Any]?) {
+        #if DEBUG
+            SDStatusBarManager.sharedInstance().enableOverrides()
+        #endif
+    }
+
+}
+
