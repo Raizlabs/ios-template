d99cad77f005de3384317395054741bdf773a0d4
Chris Ballinger <chrisballinger@gmail.com>
2018-04-18 12:56:25 -0700

    Break test on purpose

--- PRODUCTNAME/app/PRODUCTNAMETests/PRODUCTNAMETests.swift
+++ PRODUCTNAME/app/PRODUCTNAMETests/PRODUCTNAMETests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class PRODUCTNAMETests: XCTestCase {
     func testUserDefaults() {
+        // let's break a test on purpose
+        XCTAssertTrue(false)
         XCTAssertFalse(UserDefaults.hasOnboarded)
     }
 }
